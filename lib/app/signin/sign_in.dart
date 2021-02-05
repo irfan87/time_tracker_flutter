a4a5d5409ada3c7e1de6de9a6a8ba705798a5290
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-02-06 00:21:59 +0800

    sign in page UI

--- lib/app/signin/sign_in.dart
+++ lib/app/signin/sign_in.dart
@@ -1,11 +1,66 @@
 import 'package:flutter/material.dart';
+import 'package:time_tracker/common_widgets/custom_elevated_button.dart';
 
 class SignInPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
         title: Text('Time Tracker'),
+        elevation: 2.0,
+      ),
+      body: _buildContent(),
+    );
+  }
+
+  // container from scaffold
+  Widget _buildContent() {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            'Sign In',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 32.0,
+              fontWeight: FontWeight.w800,
+            ),
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          CustomElevatedButton(
+            child: Text(
+              'Sign in with Google',
+              style: TextStyle(
+                color: Colors.black87,
+                fontSize: 15.0,
+              ),
+            ),
+            color: Colors.white,
+            borderRadius: 20.0,
+            onPressed: () {},
+          ),
+          SizedBox(
+            height: 8.0,
+          ),
+          CustomElevatedButton(
+            child: Text(
+              'Sign in with Facebook',
+              style: TextStyle(
+                color: Colors.black87,
+                fontSize: 15.0,
+              ),
+            ),
+            color: Colors.white,
+            borderRadius: 20.0,
+            onPressed: () {},
+          ),
+        ],
       ),
     );
   }
---
+++ lib/common_widgets/custom_elevated_button.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class CustomElevatedButton extends StatelessWidget {
+  final Widget child;
+  final Color color;
+  final double borderRadius;
+  final VoidCallback onPressed;
+
+  CustomElevatedButton({
+    this.child,
+    this.color,
+    this.borderRadius,
+    this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: child,
+      style: ElevatedButton.styleFrom(
+        primary: color,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(
+            Radius.circular(borderRadius),
+          ),
+        ),
+      ),
+    );
+  }
+}
