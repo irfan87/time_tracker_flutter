178ae819cf6ad091a6b57163a3508c5fe33eb3af
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-02-07 22:40:06 +0800

    make a custom class for sign in button

--- lib/common_widgets/custom_elevated_button.dart
+++ lib/common_widgets/custom_elevated_button.dart
@@ -4,25 +4,30 @@ class CustomElevatedButton extends StatelessWidget {
   final Widget child;
   final Color color;
   final double borderRadius;
+  final double height;
   final VoidCallback onPressed;
 
   CustomElevatedButton({
     this.child,
     this.color,
-    this.borderRadius,
+    this.borderRadius: 20.0,
+    this.height: 50.0,
     this.onPressed,
   });
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      child: child,
-      style: ElevatedButton.styleFrom(
-        primary: color,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(
-            Radius.circular(borderRadius),
+    return SizedBox(
+      height: height,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        child: child,
+        style: ElevatedButton.styleFrom(
+          primary: color,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(borderRadius),
+            ),
           ),
         ),
       ),
