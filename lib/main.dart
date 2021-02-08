6f826303a82ed120aaae672ec3e56074d5eef92a
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-02-08 12:30:12 +0800

    setup firebase for our app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:time_tracker/app/signin/sign_in.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
   runApp(App());
 }
 
