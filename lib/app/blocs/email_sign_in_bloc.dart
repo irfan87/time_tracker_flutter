e65ad238568e67086bc9d1ef31f6c1769475ca47
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-02-20 23:23:20 +0800

    update model

--- lib/app/blocs/email_sign_in_bloc.dart
+++ lib/app/blocs/email_sign_in_bloc.dart
@@ -7,8 +7,29 @@ class EmailSignInBloc {
       StreamController<EmailSignInModel>();
 
   Stream<EmailSignInModel> get modelStream => _modelController.stream;
+  EmailSignInModel _model = EmailSignInModel();
 
   void dispose() {
     _modelController.close();
+  }
+
+  void updateWith({
+    String email,
+    String password,
+    EmailSignInFormType formType,
+    bool isLoading,
+    bool submitted,
+  }) {
+    // update model
+    _model = _model.copyWith(
+      email: email,
+      password: password,
+      formType: formType,
+      isLoading: isLoading,
+      submitted: submitted,
+    );
+
+    // add updated model to _modelController
+    _modelController.add(_model);
   }
 }
--- lib/app/models/email_sign_in_model.dart
+++ lib/app/models/email_sign_in_model.dart
@@ -17,4 +17,20 @@ class EmailSignInModel {
     this.isLoading = false,
     this.submitted = false,
   });
+
+  EmailSignInModel copyWith({
+    String email,
+    String password,
+    EmailSignInFormType formType,
+    bool isLoading,
+    bool submitted,
+  }) {
+    return EmailSignInModel(
+      email: email ?? this.email,
+      password: password ?? this.password,
+      formType: formType ?? this.formType,
+      isLoading: isLoading ?? this.isLoading,
+      submitted: submitted ?? this.submitted,
+    );
+  }
 }
