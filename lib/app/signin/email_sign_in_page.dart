8a08554912767366708412b58e10cd18ae420463
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-02-23 23:36:27 +0800

    implemented the email sign in form with ChangeNotifier

--- lib/app/signin/email_sign_in_page.dart
+++ lib/app/signin/email_sign_in_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:time_tracker/app/blocs/email_sign_in_form_bloc_base.dart';
+import 'package:time_tracker/app/blocs/email_sign_in_form_change_notifier.dart';
 
 class EmailSignInPage extends StatelessWidget {
   @override
@@ -14,7 +14,7 @@ class EmailSignInPage extends StatelessWidget {
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Card(
-            child: EmailSignInFormBlocBase.create(context),
+            child: EmailSignInFormChangeNotifier.create(context),
           ),
         ),
       ),
