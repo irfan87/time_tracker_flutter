db2856cd651633dbbaa0780416978bf5785fedad
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-02-25 16:10:35 +0800

    read and parsed the data from firestore with streams

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -5,7 +5,7 @@ import 'package:time_tracker/services/api_path.dart';
 
 abstract class Database {
   Future<void> createJob(Job job);
-  void readJobs();
+  Stream<List<Job>> jobsStream();
 }
 
 class FirestoreDatabase implements Database {
@@ -26,13 +26,24 @@ class FirestoreDatabase implements Database {
   }
 
   // read all jobs
-  void readJobs() {
+  Stream<List<Job>> jobsStream() {
     final path = APIPath.jobs(uid);
     final reference = FirebaseFirestore.instance.collection(path);
     final snapshots = reference.snapshots();
 
-    snapshots.listen((snapshot) {
-      snapshot.docs.forEach((snapshot) => print(snapshot.data()));
-    });
+    return snapshots.map(
+      (snapshot) => snapshot.docs.map(
+        (snapshot) {
+          final data = snapshot.data();
+
+          return data != null
+              ? Job(
+                  name: data['name'],
+                  ratePerHour: data['ratePerHour'],
+                )
+              : null;
+        },
+      ),
+    );
   }
 }
