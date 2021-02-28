de1f884bee25a6bcadc788bb09e91c3372a51658
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-02-28 15:39:10 +0800

    adding an empty content widget if there is no content at firebase

--- lib/app/home/jobs/jobs_page.dart
+++ lib/app/home/jobs/jobs_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:time_tracker/app/home/jobs/edit_job_page.dart';
+import 'package:time_tracker/app/home/jobs/empty_content.dart';
 import 'package:time_tracker/app/home/jobs/job_list_tile.dart';
 import 'package:time_tracker/app/home/models/job.dart';
 import 'package:time_tracker/common_widgets/show_alert_dialog.dart';
@@ -66,19 +67,24 @@ class JobsPage extends StatelessWidget {
       builder: (context, snapshot) {
         if (snapshot.hasData) {
           final jobs = snapshot.data;
-          final children = jobs
-              .map((job) => JobListTile(
-                    job: job,
-                    onTap: () => EditJobPage.show(
-                      context,
-                      job: job,
-                    ),
-                  ))
-              .toList();
 
-          return ListView(
-            children: children,
-          );
+          if (jobs.isNotEmpty) {
+            final children = jobs
+                .map((job) => JobListTile(
+                      job: job,
+                      onTap: () => EditJobPage.show(
+                        context,
+                        job: job,
+                      ),
+                    ))
+                .toList();
+
+            return ListView(
+              children: children,
+            );
+          }
+
+          return EmptyContent();
         }
 
         if (snapshot.hasError) {
