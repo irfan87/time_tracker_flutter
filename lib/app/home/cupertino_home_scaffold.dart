44e74d749ba593aa51d611d164b6b2c4de742b48
Ahmad Irfan Mohamamd Shukri <nerve2009@yahoo.com>
2021-03-03 14:27:34 +0800

    use WillPopScope widget and navigator key for android

--- lib/app/home/cupertino_home_scaffold.dart
+++ lib/app/home/cupertino_home_scaffold.dart
@@ -6,12 +6,14 @@ class CupertinoHomeScaffold extends StatelessWidget {
   final TabItem currentTab;
   final ValueChanged<TabItem> onSelectTab;
   final Map<TabItem, WidgetBuilder> widgetBuilders;
+  final Map<TabItem, GlobalKey<NavigatorState>> navigatorKeys;
 
   CupertinoHomeScaffold({
     Key key,
     @required this.currentTab,
     @required this.onSelectTab,
     @required this.widgetBuilders,
+    @required this.navigatorKeys,
   }) : super(key: key);
 
   @override
@@ -30,6 +32,7 @@ class CupertinoHomeScaffold extends StatelessWidget {
 
         return CupertinoTabView(
           builder: (context) => widgetBuilders[item](context),
+          navigatorKey: navigatorKeys[item],
         );
       },
     );
